84542465a1bab22a600b7209411bee3fa5c3d0f7
Caryolite <carl47063@gmail.com>
2025-04-20 02:40:26 +0800

    feat: 登場人物

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -23,13 +23,13 @@ const songCardList = [
 <template>
     <!-- 首頁 -->
     <div class="relative w-full h-screen overflow-hidden">
-        <div class="bg h-[1024px] w-full m-auto absolute z-[-1]"></div>
-        <img src="/img/char/tairitsu.webp" class="h-[1000px] top-[100px] -bottom-[20%]" style="left: calc(40vw - 400px);">
-        <img src="/img/char/hikari.webp" class="h-[800px] top-[200px] -bottom-[10%] z-[-1]" style="right: calc(40vw - 400px);">
-        <img src="/img/logo-tagline.svg" class="logo top-[70%] left-[50%]">
-        <img src="/img/logo-homepage.png" class="logo h-[200px] top-[85%] left-[50%]">
-        <a href="#"><img src="/img/google-play.png" class="link bottom-[40px] right-[8vw]"></a>
-        <a href="#"><img src="/img/appStore.png" class="link absolute bottom-[90px] right-[8vw]"></a>
+        <div class="h-[1024px] w-full m-auto absolute z-[-1] bg-[url(/img/bg/main_bg.jpg)] bg-center bg-cover bg-no-repeat"></div>
+        <img src="/img/char/tairitsu.webp" class="absolute h-[1000px] top-[100px] -bottom-[20%]" style="left: calc(40vw - 400px);">
+        <img src="/img/char/hikari.webp" class="absolute h-[800px] top-[200px] -bottom-[10%] z-[-1]" style="right: calc(40vw - 400px);">
+        <img src="/img/logo-tagline.svg" class="logo absolute top-[70%] left-[50%]">
+        <img src="/img/logo-homepage.png" class="logo absolute h-[200px] top-[85%] left-[50%]">
+        <a href="#"><img src="/img/google-play.png" class="absolute bottom-[40px] right-[8vw]"></a>
+        <a href="#"><img src="/img/appStore.png" class="absolute absolute bottom-[90px] right-[8vw]"></a>
     </div>
     <!-- 遊戲曲目 -->
     <div class="w-full h-[600px]">
@@ -39,48 +39,41 @@ const songCardList = [
         <div class="w-[85%] pt-[36px] m-auto flex justify-between overflow-hidden">
 
             <div v-for="(song, index) in songCardList" :key="index">
-                <SongCard :title="song.title" :artist="song.artist" :url="song.url"/>
+                <SongCard :title="song.title" :artist="song.artist" :url="song.url" />
             </div>
+
         </div>
     </div>
     <!-- 登場人物 -->
-     <div class="w-full h-[700px] relative">
+    <div class="w-full h-[700px] relative">
         <div class="s3bg w-full h-full absolute z-[-1] bg-[url(/img/bg/s3_bg_dark.jpg)] bg-center bg-cover bg-no-repeat"></div>
-        <h2><span>登場人物</span></h2>
+        <h2 class="font-[900] text-[36px] pt-[40px]">
+            <span class="char-title">登場人物</span>
+        </h2>
         <h3>CHARACTERS</h3>
-        <div class="char_Q">
-            <div class="char_Hikari">
-                <h4>光</h4>
-                <h5>HIKARI</h5>
+        <div class="w-full mt-[40px] overflow-hidden flex justify-center gap-[20px]">
+            <div class="self-end text-[40px] font-[900] mx-20[px]">
+                <h4 style="text-align: right;">光</h4>
+                <h5 class="pb-[40px] text-[#F95499]">HIKARI</h5>
             </div>
-            <img src="/img/char/Hikari_Q.webp" alt="" class="hikari_Q">
-            <img src="/img/char/Tairitsu_Q.webp" alt="" class="tairitsu_Q">
-            <div class="char_Tairitsu">
-                <h4>對立</h4>
-                <h5>TAIRITSU</h5>
+            <img src="/img/char/Hikari_Q.webp" class="char relative h-[400px] w-auto">
+            <img src="/img/char/Tairitsu_Q.webp" class="char relative h-[400px] w-auto">
+            <div class="self-end text-[40px] font-[900] mx-20[px]">
+                <h4 style="text-align: left;">對立</h4>
+                <h5 class="pb-[40px] text-[#8F12E1]">TAIRITSU</h5>
             </div>
         </div> 
-     </div>
+    </div>
 </template>
 
 <style scoped>
-.bg{
-    background-image: url(/img/bg/main_bg.jpg);
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-}
-img{
-    position: absolute;
-}
 .logo{
     z-index: 2;
     transform: translate(-50%, -50%);
 }
-h2,h3,h4,h5,p{
+h2, h3, h4, h5, p{
    margin: 0;
    text-align: center;
-   color: var(--white);
 }
 h2{
     font-weight: 600;
@@ -88,27 +81,18 @@ h2{
     padding-top: 50px;
 }
 h3{
-    font-weight: 400;
+    font-weight: 300;
     padding-bottom: 60px;
 }
-.song-card{
-    height: 290px;
-    width: 220px;
-    position: relative;
+.char-title{
+    background-image: linear-gradient(45deg, #F95499, #8F12E1);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -moz-background-clip: text;
+    -webkit-text-fill-color: transparent; 
+    -moz-text-fill-color: transparent;
 }
-.song-card div{
-    width: 220px;
-    height: 220px;
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-}
-.song-card h4{
-    margin: 10px 0;
-    font-size: 18px;
-    font-weight: bold;
-}
-.song-card h5{
-    font-weight: 200;
+.char{
+    filter: drop-shadow(0 0 10px #000);
 }
 </style>
