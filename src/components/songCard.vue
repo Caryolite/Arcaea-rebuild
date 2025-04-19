745f4e0c124ad5b07781b73bfc2b6b37678e4302
Caryolite <carl47063@gmail.com>
2025-04-20 02:39:14 +0800

    fix: 修正songCard字體樣式

--- src/components/songCard.vue
+++ src/components/songCard.vue
@@ -2,12 +2,11 @@
   <div class="h-[290px] w-[220px] relative">
         <div class="w-[220px] h-[220px] bg-center bg-cover bg-no-repeat"  :style="{ backgroundImage: 'url(' + url + ')' }"></div>
         <h4 class="my-[10px] text-center text-[18px] font-bold">{{ title }}</h4>
-        <h5 class="my-[10px] text-center">{{ artist }}</h5>
+        <h5 class="font-[300] text-center">{{ artist }}</h5>
     </div>
 </template>
 
 <script setup>
-import { defineProps } from 'vue'
 
 const props = defineProps({
   title: String,
