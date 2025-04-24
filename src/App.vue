c1c2d49dd4ca0ff96c4dca1818b971dbfd7b4a22
Caryolite <carl47063@gmail.com>
2025-04-24 23:08:18 +0800

    feat: story

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,17 @@
 import Nav from './components/nav.vue'
 import Main from './components/Main.vue'
 import Footer from './components/footer.vue'
+import story from './components/story.vue'
+
+import { ref } from 'vue'
+const pageIndex = ref(1);
+
 </script>
 
 <template>
-  <Nav class="w-full z-[1] over-hidden"/>
-  <Main class="w-full overflow-hidden"/>
+  <Nav class="w-full z-[1] over-hidden" />
+  <Main class="w-full overflow-hidden" />
+  <!-- <story class="relative" /> -->
   <Footer/>
 </template>
 
---
+++ src/components/story.vue
@@ -0,0 +1,45 @@
+<template>
+  
+<div class="relative w-full h-full overflow-hidden">
+<div class="story_bg w-full h-full absolute z-[-1] bg-[url(/img/bg/bg05.jpg)] bg-center bg-cover bg-no-repeat  brightness-[0.2]"></div>
+<div class="w-[80%] m-auto pt-[30px]">  
+
+    <div class="w-[80%] m-auto pt-[100px] pb-[360px] text-[1.2rem]">
+    <div class="py-[36px]">
+    <p>
+        兩位少女探索一個支離破碎的世界。那裡伴隨著一個聲音：一段塵封的過往即將被揭開... <br><br>
+        她們從這個除了廢墟一無所有的世界中醒來，卻絲毫不記得這裡曾發生過的一切。這一刻她們才發現，自己也不過是虛無的存在。<br><br>
+        而接下來，她們會發現一個名叫Arcaea的物體。它由空中漂浮著玻璃質感般的碎片所積聚而成，而這每一塊碎片，都包含著來自過去的鮮活回憶。
+    </p>
+    </div>
+    <div class="py-[36px]"><p>
+        這些碎片向兩個少女身邊靠攏，似乎就像是它們在選擇讓誰來體驗這段塵封的記憶。<br><br>
+        而少女們也通過自己的方式，體驗到了完全不同的過去。<br><br>
+        這些玻璃反射出了另一個純潔的世界。她從中看見海洋、都市、火焰、光芒；美好的景象目不暇接。她抬起了自己的手，試圖去抓住它們，她開心地笑了出來。<br><br>
+        就像透過火車車窗看外頭稍縱即逝的景色一般， 她瞧見了陰雨的景象。下一次是豔陽。再下一次是死亡。她厭惡地遠離了這些碎片。
+    </p></div>
+    <div class="py-[36px]"><p>
+        兩個少女在探索著支離破碎的世界中逐漸解鎖那塵封的過去。<br><br>
+        一側是光，另一側是對立。<br><br>
+        她們在Arcaea世界中所探索的一切會改變她們嗎？<br><br>
+        這美妙而又簡簡單單的世界，只需要令人感到愉快就足够了。沒錯，無需多求。<br><br>
+        但無論這些碎片在反射什麼……她從中未見到一絲光芒。哪怕是最小的火花，都會在一瞬間消失於她的視線。<br><br>
+        而這之後，在旅途的盡頭，她們又會變成誰？
+    </p></div>
+    </div>
+
+</div>  
+</div>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+p{
+    font-family: "Inter", "Noto Sans TC", sans-serif;
+    font-weight: 100;
+}
+</style>
