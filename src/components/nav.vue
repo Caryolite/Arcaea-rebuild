68ff35188049c5fa2ceb38c4e438f52b08211ba4
Caryolite <carl47063@gmail.com>
2025-04-24 22:29:45 +0800

    feat: nav hover特效

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { ref } from 'vue'
 
 </script>
 
@@ -7,13 +6,13 @@ import { ref } from 'vue'
 
 <div class="absolute top-[30px] ">
 <header class="w-[80%] m-auto flex justify-between align-center items-center">
-  <div class="float-left"><img src="/img/logo-dark-bg.svg" alt=""></div>
+  <div class="float-left"><img src="/img/logo-dark-bg.svg"></div>
   <ul class="flex justify-end float-right m-0">
     <li><a href="">樂曲排名</a></li>
     <li><a href="">故事</a></li>
     <li><a href="">周邊</a></li>
-    <li class="pl-[160px] relative">
-        <a href=""><img src="/img/nav-login.svg" alt="" class="w-[140px] h-auto z-[-1] -top-[10px]"></a>
+    <li class="login pl-[160px] relative">
+        <a href=""><img src="/img/nav-login.svg" class="w-[140px] h-auto z-[-1] -top-[10px]"></a>
     </li>
   </ul>
 </header></div>
@@ -35,5 +34,15 @@ header li{
 }
 header li img{
   right: calc(5%);
+}
+li:hover{
+  border-bottom: 2px solid var(--white);
+  padding-bottom: 18px;
+}
+.login:hover{
+  border-bottom: 0;
+  padding: 20px 0;
+  filter: drop-shadow(0 0 15px var(--purple));
+  transition-duration: 200ms;
 }
 </style>
